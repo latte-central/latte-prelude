661d00204a7b875c6af20b33e5e81216a1680b50
Frederic Peschanski <fredokun@github.com>
2020-07-29 16:46:27 +0200

    programmatic require

--- src/latte_prelude/main.clj
+++ src/latte_prelude/main.clj
@@ -1,11 +1,6 @@
 (ns latte-prelude.main
   (:gen-class)
-  (:require [latte.main :refer [latte-main]]
-            [latte-prelude.prop]
-            [latte-prelude.equal]
-            [latte-prelude.quant]
-            [latte-prelude.classic]
-            [latte-prelude.fun]))
+  (:require [latte.main :refer [latte-main]]))
 
 (defn -main [& args]
   (latte-main args "latte-prelude"
