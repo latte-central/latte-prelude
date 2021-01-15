13bda0feb4b9d80de08104c6774a776913d8afa3
Fredokun <frederic.peschanski@lip6.fr>
2021-01-15 18:21:38 +0100

    deps version bump

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [latte "1.0b9-SNAPSHOT"]]
+                 [latte "1.0b10-SNAPSHOT"]]
   :main latte-prelude.main
   :aliases {"certify" ["run" ":certify"]
             "clear-cert" ["run" ":clear-cert"]}
