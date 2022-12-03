7cc89e53d3537a8b04c39997f220f9b8c7571ddd
mrgraal <frederic.peschanski@lip6.fr>
2022-12-03 18:17:19 +0100

    more algebra

--- src/latte_prelude/algebra.clj
+++ src/latte_prelude/algebra.clj
@@ -1,18 +1,19 @@
 (ns latte-prelude.algebra
   "Basic algebraic definitions."
 
-  (:refer-clojure :exclude [and or not])
+  (:refer-clojure :exclude [and or not identity])
   
   (:require
    [latte-kernel.norm :as norm]
    
    [latte.utils :refer [decomposer set-opacity!]]
    [latte.core :as latte :refer [definition defthm defimplicit defimplicit*
-                                 assume have qed proof]]
+                                 assume have qed proof try-proof]]
    
    [latte-prelude.utils :as pu]
-   [latte-prelude.prop :as p :refer [<=> and or not]]
-   [latte-prelude.equal :as eq :refer [equal]])
+   [latte-prelude.prop :as p :refer [<=> and and* or not]]
+   [latte-prelude.equal :as eq :refer [equal]]
+   [latte-prelude.quant :as q :refer [single]])
 )
 
 
@@ -31,7 +32,43 @@ This makes the type `T` associated to the law a *semigroup*."
 
 
 (definition identity-right
-  "The property of an element `e` to be a left-identity of a law `⋅`."
+  "The property of an element `e` to be a right-identity of a law `⋅`."
   [[?T :type] [⋅ (==> T T T)] [e T]]
   (∀ [x T] (equal (⋅ x e) x)))
+
+(definition identity
+  "The property of an element `e` to be an identity of a law `⋅`"
+  [[?T :type] [⋅ (==> T T T)] [e T]]
+  (and (identity-left ⋅ e)
+       (identity-right ⋅ e)))
+
+(defthm identity-single
+  "There is at most one identity element of a law `⋅`"
+  [[?T :type] [⋅ (==> T T T)]]
+  (single (identity-def T ⋅)))
+
+(proof 'identity-single-thm
+  (assume [e1 T
+           e2 T
+           He1 (identity ⋅ e1)
+           He2 (identity ⋅ e2)]
+    (have <a> (equal (⋅ e1 e2) e2)
+          :by ((p/and-elim-left He1) e2))
+    (have <b> (equal (⋅ e1 e2) e1)
+          :by ((p/and-elim-right He2) e1))
+    (have <c> (equal e1 e2)
+          :by (eq/eq-trans (eq/eq-sym <b>)
+                           <a>)))
+  (qed <c>))
+
+(definition monoid
+  "The law `⋅` together with the unit element `e` for a monoid."
+  [[?T :type] [⋅ (==> T T T)] [e T]]
+  (and* (associative ⋅)
+        (identity-left ⋅ e)
+        (identity-right ⋅ e)))
+
+
+           
+
 
--- src/latte_prelude/main.clj
+++ src/latte_prelude/main.clj
@@ -4,7 +4,12 @@
 
 (defn -main [& args]
   (latte-main args "latte-prelude"
-              '[latte-prelude.prop latte-prelude.equal latte-prelude.quant latte-prelude.classic latte-prelude.fun]))
+              '[latte-prelude.prop
+                latte-prelude.equal
+                latte-prelude.quant
+                latte-prelude.classic
+                latte-prelude.fun
+                latte-prelude.algebra]))
 
 
 
